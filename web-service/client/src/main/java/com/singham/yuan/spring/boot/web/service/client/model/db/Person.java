224a775cefadac9c8ecac39cd85a278337420a4a
singham.yuan <singham.yuan@derbysoft.com>
2018-03-07 16:49:00 +0800

    add new files

--- web-service/client/src/main/java/com/singham/yuan/spring/boot/web/service/client/model/db/Person.java
+++ web-service/client/src/main/java/com/singham/yuan/spring/boot/web/service/client/model/db/Person.java
@@ -1,5 +1,8 @@
 package com.singham.yuan.spring.boot.web.service.client.model.db;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.Date;
 
@@ -9,55 +12,31 @@ import java.util.Date;
         @Index(columnList = "name", name = "name")})
 public class Person {
 
+    @Getter
+    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
     private String name;
 
+    @Getter
+    @Setter
     @Temporal(TemporalType.TIMESTAMP)
     @Column(nullable = false)
     private Date createTime = new Date();
 
+    @Getter
+    @Setter
     @Temporal(TemporalType.TIMESTAMP)
     @Column(nullable = false)
     private Date lastUpdateTime = createTime;
 
     public Person(String name) {
         this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public Date getLastUpdateTime() {
-        return lastUpdateTime;
-    }
-
-    public void setLastUpdateTime(Date lastUpdateTime) {
-        this.lastUpdateTime = lastUpdateTime;
     }
 
 }
